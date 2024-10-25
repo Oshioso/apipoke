5af982d1b456b90365f088d7712642bd32ec5188
Oshioso <163473127+Oshioso@users.noreply.github.com>
2024-10-25 14:18:31 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const card_atutomatico = document.querySelector(".container_pokemon")
+const poke_container = document.querySelector(".container_pokemon")
 
 const pokemonConut  = 40;
 
@@ -17,28 +17,30 @@ const fetchPokemon = async (id) => {
     const data = await APIResponse.json();
 
     create_pokemon_card(data)
+
+    //.sprites.type[0].url
   
 }
 
 const create_pokemon_card = (poke) => {
     
-    console.log(poke)
+    console.log(poke.types[0].type.url)
 
     const card_div = document.createElement('div')
-    card_div.classList(".card_pokemon")
+    card_div.classList.add("card_pokemon")
 
-    const name = poke.name[0].toUpperCase() + pokemonConut.name.slice(i)
-    const id = poke.id.tostring().padStart(3, '0')
+    const name = poke.name[0].toUpperCase() + poke.name.slice(1)
+    const id = poke.id.toString().padStart(3, '0')
 
     const type = poke.types.map(type => type.type.name)
     console.log(type)
 
-    const innerHTML = `
-        <img class="pokemon_img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/717.png" alt="">
+    const pokeinnerHTML = `
+        <img class="pokemon_img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png" alt="${name}">
 
         <h1>
-            <span class="pokemon_nunber">#857</span>
-            <span class="pokemon_name">yveltal</span>
+            <span class="pokemon_nunber">#${id}</span>
+            <span class="pokemon_name">${poke.name}</span>
         </h1>
 
         <div class="pokemon_type">
@@ -47,9 +49,11 @@ const create_pokemon_card = (poke) => {
         </div>
     
     `
+    card_div.innerHTML = pokeinnerHTML
 
+    card_div.classList.add("card_pokemon")
 
-    card_div.classList(".card_pokemon")
+    poke_container.appendChild(card_div)
 
 }
 
