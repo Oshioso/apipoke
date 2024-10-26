ad0d9895ae2ac7900cceabe55540ec843b36244e
Oshioso <163473127+Oshioso@users.noreply.github.com>
2024-10-25 21:08:48 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,61 +1,57 @@
-const poke_container = document.querySelector(".container_pokemon")
-
-const pokemonConut  = 40;
-
-
+const poke_container = document.querySelector(".container_pokemon");
+const pokemonCount = 100;
 
 const getPokemons = async () => {
-    for (let i = 1; i <= pokemonConut; i++){
-        await fetchPokemon(i)
+    for (let i = 1; i <= pokemonCount; i++) {
+        await fetchPokemon(i);
     }
-
-}
+};
 
 const fetchPokemon = async (id) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-  
     const data = await APIResponse.json();
 
-    create_pokemon_card(data)
+    // Itera sobre os tipos do Pokémon e faz uma nova requisição para cada tipo
+    const typesInfo = await Promise.all(
+        data.types.map(async (type) => {
+            const typeResponse = await fetch(type.type.url);
+            const typeData = await typeResponse.json();
+            const typeIconUrl = typeData.sprites["generation-viii"]["sword-shield"].name_icon; // Captura o ícone do tipo específico
+            console.log(type)
+            return { name: type.type.name, iconUrl: typeIconUrl }; // Retorna nome e URL do ícone
+        })
+    );
 
-    //.sprites.type[0].url
-  
-}
+    createPokemonCard(data, typesInfo);
+};
 
-const create_pokemon_card = (poke) => {
+const createPokemonCard = (poke, typesInfo) => {
+    const card_div = document.createElement('div');
+    card_div.classList.add("card_pokemon");
+
+    const name = poke.name[0].toUpperCase() + poke.name.slice(1);
     
-    console.log(poke.types[0].type.url)
+    const id = poke.id.toString().padStart(3, '0');
 
-    const card_div = document.createElement('div')
-    card_div.classList.add("card_pokemon")
-
-    const name = poke.name[0].toUpperCase() + poke.name.slice(1)
-    const id = poke.id.toString().padStart(3, '0')
-
-    const type = poke.types.map(type => type.type.name)
-    console.log(type)
+    // Criando os ícones de tipo dinamicamente com URL do ícone específico
+    const types = typesInfo.map(type => {
+        return `<img src="${type.iconUrl}" alt="${type.name}">`;
+    }).join("");
 
     const pokeinnerHTML = `
         <img class="pokemon_img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png" alt="${name}">
 
         <h1>
-            <span class="pokemon_nunber">#${id}</span>
-            <span class="pokemon_name">${poke.name}</span>
+            <span class="pokemon_number">#${id}</span>
+            <span class="pokemon_name">${name}</span>
         </h1>
 
         <div class="pokemon_type">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-viii/sword-shield/17.png" alt="">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-viii/sword-shield/3.png" alt="">
+            ${types}
         </div>
-    
-    `
-    card_div.innerHTML = pokeinnerHTML
+    `;
+    card_div.innerHTML = pokeinnerHTML;
+    poke_container.appendChild(card_div);
+};
 
-    card_div.classList.add("card_pokemon")
-
-    poke_container.appendChild(card_div)
-
-}
-
-
-getPokemons()
+getPokemons();
